6e86265306a8c3dd362d46e462515fcbc76a0afb
armanmoradi <armanmoradi8@gmail.com>
2024-06-20 02:06:54 +0330

    add new root and add path for passwords

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :passwords
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # root to: "home#index"
+  root to: "passwords#index"
 end
