7e053230895fcfa3b0e0ccd3fbd924b6fd1647e4
armanmoradi <armanmoradi8@gmail.com>
2024-06-23 01:54:07 +0330

    create share controller and edit form parshal

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :passwords
+  resources :passwords do
+    resources :shares, only: [:new, :create, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root to: "passwords#index"
 end
