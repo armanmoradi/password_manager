8c37bec365e362364c6aa36dad2df08138094efc
armanmoradi <armanmoradi8@gmail.com>
2024-06-24 02:12:06 +0330

    update more validate for delete and view password

--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -2,10 +2,40 @@ class UserPassword < ApplicationRecord
   # Owner view, edit, and share the password
   # View can only view the password
   # Editor can update password
-  ROLES = %w{owner viewer editor}
+  ROLES = %w{owner editor viewer}
 
   belongs_to :user
   belongs_to :password
 
   validates :role, presence: :true, inclusion: { in: ROLES }
+
+  attribute :role, default: :viewer
+
+
+  def editable?
+    owner? || editor?
+  end
+
+  def shareable?
+    owner?
+  end
+
+  def deletable?
+    owner?
+  end
+
+  private
+
+  def owner?
+    role == "owner"
+  end
+
+  def viewr?
+    role == "viwer"
+  end
+
+  def editor?
+    role == "editor"
+  end
+
 end
