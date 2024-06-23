ffecb1878dca517999d67c614de0cbe68b0b65d8
armanmoradi <armanmoradi8@gmail.com>
2024-06-23 23:07:27 +0330

    add role to UserPassword model

--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -1,4 +1,11 @@
 class UserPassword < ApplicationRecord
+  # Owner view, edit, and share the password
+  # View can only view the password
+  # Editor can update password
+  ROLES = %w{owner viewer editor}
+
   belongs_to :user
   belongs_to :password
+
+  validates :role, presence: :true, inclusion: { in: ROLES }
 end
